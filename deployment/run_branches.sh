cac0e228793239d0e0977efee203bb625df256b3
IreneGR92 <42998147+IreneGR92@users.noreply.github.com>
2019-01-06 16:44:30 +0100

    branches update

--- deployment/run_branches.sh
+++ deployment/run_branches.sh
@@ -4,6 +4,17 @@
 declare -a arr=("supercomputer_conf_NRN_m02"
                 "supercomputer_conf_dispersal003"
                 "supercomputer_config_NRN_K1-2"
+		"supercomputer_config_NRN_Xsn3"
+		"supercomputer_config_Xsh4"
+		"supercomputer_config_Xsh5"
+		"supercomputer_config_NRN_bias10"
+		"supercomputer_config_NRN_Xsn06"
+		"supercomputer_config_NRN_Xsh5"
+		"supercomputer_config_NRN_Xsh4"
+		"supercomputer_config_m02_bias10_longer"
+		"supercomputer_config_Xsn06"
+		"supercomputer_config_NRN_dispersal003"
+		"supercomputer_config_NRN_m02_bias10"
                 )
 cd ../../
 for i in "${arr[@]}"
