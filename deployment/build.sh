47c4b01057756c51ae1f0011afdf8f477f345d7b
García Ruiz Irene (ig17c521) <ig17c521@submit01.ubelix.unibe.ch>
2019-01-06 17:06:40 +0100

    fixed module load issue

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 git pull
+module load CMake
 echo "cleaning up build folder"
 rm -rf ../build
 mkdir ../build
@@ -12,7 +13,7 @@ echo "running software"
 
 
 echo "copy results"
-mv group_augmentation*.txt ../../results/results/last_generation
+mv group_augmentation_last_generation*.txt ../../results/results/last_generation
 mv group_augmentation*.txt ../../results/results/main
 cd ../../results/
 echo "git stuff"
