030cf3a7da5c36a0d6b2fe35e19b0c638beb1200
IreneGR <irene.garcia@iee.unibe.ch>
2019-02-19 19:07:05 +0100

    updated run_branches for new formula of survival

--- deployment/run_branches.sh
+++ deployment/run_branches.sh
@@ -2,25 +2,17 @@
 
 
 declare -a arr=("supercomputer_init"
-
-                "supercomputer_m015"
-
+                "supercomputer_X0-0.6_Xsh-0.5_Xsn-0.5"
+                "supercomputer_X0-0.5_Xsh-0.5_Xsn-0.5"
+                "supercomputer_X0-0.7_Xsh-0.5_Xsn-0.3"
+                "supercomputer_X0-0.5_Xsh-0.3_Xsn-0.5"
+                "supercomputer_X0-0.7_Xsh-0.1_Xsn-0"
                 "supercomputer_bias10"
                 "supercomputer_K1-2"
-                "supercomputer_Xsh1"
-                "supercomputer_Xsn1"
-                "supercomputer_Xsn0"
+                "supercomputer_K1-2_X0-0.5"
+                "supercomputer_K1-0.5"
                 "supercomputer_dispersal1"
-                "supercomputer_NRN"
-
-                "supercomputer_NRN_m015"
-
-                "supercomputer_NRN_bias10"
-                "supercomputer_NRN_K1-2"
-                "supercomputer_NRN_Xsh1"
-                "supercomputer_NRN_Xsn1"
-                "supercomputer_NRN_Xsn0"
-                "supercomputer_NRN_dispersal1")
+                "supercomputer_dispersal0")
 cd ../../
 for i in "${arr[@]}"
 do
