ee06ccd3dc06d01e6c2acea11d7a840482d37b4b
IreneGR92 <42998147+IreneGR92@users.noreply.github.com>
2019-02-24 13:39:15 +0100

    fix branches.sh

--- deployment/run_branches.sh
+++ deployment/run_branches.sh
@@ -3,8 +3,8 @@
 
 declare -a arr=("supercomputer_X07_Xh02_Xn0"
                 "supercomputer_X07_Xh04_Xn02"
-                "supercomputer_X06_Xh02_Xn04
-                "supercomputer_X06
+                "supercomputer_X06_Xh02_Xn04"
+                "supercomputer_X06"
                 "supercomputer_X05")
 cd ../../
 for i in "${arr[@]}"
