e7be388e4a2fef5ed4598113ed56c907b6593025
IreneGR <irene.garcia@iee.unibe.ch>
2019-03-06 18:22:43 +0100

    update branches.sh

--- deployment/run_branches.sh
+++ deployment/run_branches.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
 
-declare -a arr=("supercomputer__X05_Xh02_Xn04_longer")
+declare -a arr=("supercomputer_dispersal1_NRN"
+		"supercomputer_X05_Xh02_Xn04_NRN"
+		"supercomputer_X05_Xh03_Xn05"
+"supercomputer_low_relatedness_NRN"
+"supercomputer_low_relatedness2"
+"supercomputer_X06_Xh02_Xn04_NRN"
+"supercomputer_bias10_NRN"
+"supercomputer_K05_NRN"
+"supercomputer_X05_K2_NRN"
+"supercomputer_dispersal005_NRN"
+"supercomputer_K2_NRN"
+"supercomputer_X07_Xh02_Xn0_NRN"
+"supercomputer_X_Xh_Xn_NRN"
+"supercomputer_init"
+"supercomputer_X5_bias10_NRN"
+"supercomputer_X05_K05_NRN"
+"supercomputer_supercomputer_X07_Xh04_Xn02_NRN")
+
 cd ../../
 for i in "${arr[@]}"
 do
