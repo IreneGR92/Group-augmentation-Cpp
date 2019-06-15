87831526599c58ab0d3eedb6ccfb32c4a4f65a0f
tobakk <17847628+tobakk@users.noreply.github.com>
2019-06-15 12:26:20 +0200

    remove successfull branches

--- deployment/run_branches.sh
+++ deployment/run_branches.sh
@@ -4,16 +4,10 @@ project_root=~/git/main/
 git_root=~/git/
 
 declare -a arr=("sc-init"
-		"sc-X07-Xh04-Xn02"
 		"sc-X07-Xh02-Xn02"
-		"sc-X07-Xh02-Xn0"
-		"sc-X05-Xh04-Xn04"
 		"sc-X05-Xh02-Xn04"
 		"sc-K2"
-		"sc-K2-Xn02"
 		"sc-K2-Xh02-Xn02"
-		"sc-K2-Xh02-Xn0"
-		"sc-K2-X05"
 		"sc-K2-X05-Xh02"
 		"sc-bias10"
 		"sc-bias10-Xn02"
