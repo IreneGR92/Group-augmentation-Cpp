c2148e97094594bd0267e7452e0dad29e30e6f3c
tobakk <17847628+tobakk@users.noreply.github.com>
2019-06-26 00:57:50 +0200

    add ubelix slurm task

--- deployment/ubelix.sh
+++ deployment/ubelix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=test_omp
+#SBATCH --job-name=group-augmentation
 #SBATCH --output=res_omp.txt
 #
 #SBATCH --ntasks=1
@@ -8,29 +8,92 @@
 #SBATCH --time=10:00
 #SBATCH --mem-per-cpu=100
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-./hello.omp
+
+declare -a arr=("init-No_relatedness.yml"
+		"07-Xh04-Xn02-No_relatedness.yml"
+		"X07-Xh02-Xn02-No_relatedness.yml"
+		"X07-Xh02-Xn0-No_relatedness.yml"
+		"X05-Xh04-Xn04-No_relatedness.yml"
+		"X05-Xh02-Xn04-No_relatedness.yml"
+		"K2-No_relatedness.yml"
+		"K2-Xn02-No_relatedness.yml"
+		"K2-Xh02-Xn02-No_relatedness.yml"
+		"K2-Xh02-Xn0-No_relatedness.yml"
+		"K2-X05-No_relatedness.yml"
+		"K2-X05-Xh02-No_relatedness.yml"
+		"bias10-No_relatedness.yml"
+		"bias10-Xn02-No_relatedness.yml"
+		"bias10-Xh02-Xn02-No_relatedness.yml"
+		"bias10-Xh02-Xn0-No_relatedness.yml"
+		"bias10-X05-No_relatedness.yml"
+		"bias10-X05-Xh02-No_relatedness.yml"
+		"bias10-X05-Xh02-Xn02-No_relatedness.yml"
+		"bias10-K2-No_relatedness.yml"
+		"bias10-K2-Xn02-No_relatedness.yml"
+		"bias10-K2-Xh02-Xn02-No_relatedness.yml"
+		"bias10-K2-Xh02-Xn0-No_relatedness.yml"
+		"bias10-K2-X05-No_relatedness.yml"
+		"bias10-K2-X05-Xh02-No_relatedness.yml"
+		"bias10-K2-X05-Xh02-Xn02-No_relatedness.yml"
+
+		"init-NRN.yml"
+		"X07-Xh04-Xn02-NRN.yml"
+		"X07-Xh02-Xn02-NRN.yml"
+		"X07-Xh02-Xn0-NRN.yml"
+		"X05-Xh04-Xn04-NRN.yml"
+		"X05-Xh02-Xn04-NRN.yml"
+		"K2-NRN.yml"
+		"K2-Xn02-NRN.yml"
+		"K2-Xh02-Xn02-NRN.yml"
+		"K2-Xh02-Xn0-NRN.yml"
+		"K2-X05-NRN.yml"
+		"K2-X05-Xh02-NRN.yml"
+		"bias10-NRN.yml"
+		"bias10-Xn02-NRN.yml"
+		"bias10-Xh02-Xn02-NRN.yml"
+		"bias10-Xh02-Xn0-NRN.yml"
+		"bias10-X05-NRN.yml"
+		"bias10-X05-Xh02-NRN.yml"
+		"bias10-X05-Xh02-Xn02-NRN.yml"
+		"bias10-K2-NRN.yml"
+		"bias10-K2-Xn02-NRN.yml"
+		"bias10-K2-Xh02-Xn02-NRN.yml"
+		"bias10-K2-Xh02-Xn0-NRN.yml"
+		"bias10-K2-X05-NRN.yml"
+		"bias10-K2-X05-Xh02-NRN.yml"
+		"bias10-K2-X05-Xh02-Xn02-NRN.yml"
+
+		"init-RN.yml"
+		"X07-Xh04-Xn02-RN.yml"
+		"X07-Xh02-Xn02-RN.yml"
+		"X07-Xh02-Xn0-RN.yml"
+		"X05-Xh04-Xn04-RN.yml"
+		"X05-Xh02-Xn04-RN.yml"
+		"K2-RN.yml"
+		"K2-Xn02-RN.yml"
+		"K2-Xh02-Xn02-RN.yml"
+		"K2-Xh02-Xn0-RN.yml"
+		"K2-X05-RN.yml"
+		"K2-X05-Xh02-RN.yml"
+		"bias10-RN.yml"
+		"bias10-Xn02-RN.yml"
+		"bias10-Xh02-Xn02-RN.yml"
+		"bias10-Xh02-Xn0-RN.yml"
+		"bias10-X05-RN.yml"
+		"bias10-X05-Xh02-RN.yml"
+		"bias10-X05-Xh02-Xn02-RN.yml"
+		"bias10-K2-RN.yml"
+		"bias10-K2-Xn02-RN.yml"
+		"bias10-K2-Xh02-Xn02-RN.yml"
+		"bias10-K2-Xh02-Xn0-RN.yml"
+		"bias10-K2-X05-RN.yml"
+		"bias10-K2-X05-Xh02-RN.yml"
+		"bias10-K2-X05-Xh02-Xn02-RN.yml")
+
 
 
 
 for i in "${arr[@]}"
 do
-folder_name="auto_$i"
-
-    echo deleting folder ${folder_name}
-    rm -rf ${folder_name}
-    pwd
-    echo cloning "${folder_name}"
-    pwd
-    git clone -b ${i} --single-branch git@github.com:IreneGR92/Group-augmentation-RN.git ${folder_name}
-    cd ${folder_name}
-    pwd
-    git checkout ${i}
-    echo running "${folder_name}"
-    cd deployment
-    chmod +x build.sh
-    pwd
-    screen -d -S "${folder_name}" -m ./build.sh
-    cd ../..
-    pwd
+srun "../bin/App ../parameters/${i}"
 done
