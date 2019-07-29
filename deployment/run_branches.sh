e0ca622b19c4f774df056e43cc90a22a8f6ac4cf
IreneGR <irene.garcia@iee.unibe.ch>
2019-07-29 12:07:04 +0200

    Updated ylm files for NRN and RN. Now default includes evolution help after dispersal

--- deployment/run_branches.sh
+++ deployment/run_branches.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 declare -a arr=(
-#       "init-No_relatedness.yml"
+#   "init-No_relatedness.yml"
 #		"07-Xh04-Xn02-No_relatedness.yml"
 #		"X07-Xh02-Xn02-No_relatedness.yml"
 #		"X07-Xh02-Xn0-No_relatedness.yml"
@@ -65,7 +65,7 @@ declare -a arr=(
 #		"K2-Xn02-RN.yml"
 #		"K2-Xh02-Xn02-RN.yml"
 #		"K2-Xh02-Xn0-RN.yml"
-##		"K2-X05-RN.yml"
+#		"K2-X05-RN.yml"
 #		"K2-X05-Xh02-RN.yml"
 #		"bias10-RN.yml"
 #		"bias10-Xn02-RN.yml"
@@ -82,27 +82,6 @@ declare -a arr=(
 #		"bias10-K2-X05-Xh02-RN.yml"
 #		"bias10-K2-X05-Xh02-Xn02-RN.yml"
 
-		"init-No_help.yml"
-		"X07-Xh04-Xn02-No_help.yml"
-		"X07-Xh02-Xn02-No_help.yml"
-		"X05-Xh04-Xn04-No_help.yml"
-		"X05-Xh02-Xn04-No_help.yml"
-		"K2-No_help.yml"
-		"K2-Xn02-No_help.yml"
-		"K2-Xh02-Xn02-No_help.yml"
-		"K2-X05-No_help.yml"
-		"K2-X05-Xh02-No_help.yml"
-
-		"init-RN-No_help.yml"
-		"X07-Xh04-Xn02-RN-No_help.yml"
-		"X07-Xh02-Xn02-RN-No_help.yml"
-		"X05-Xh04-Xn04-RN-No_help.yml"
-		"X05-Xh02-Xn04-RN-No_help.yml"
-		"K2-RN-No_help.yml"
-		"K2-Xn02-RN-No_help.yml"
-		"K2-Xh02-Xn02-RN-No_help.yml"
-		"K2-X05-RN-No_help.yml"
-		"K2-X05-Xh02-RN-No_help.yml"
 		)
 
 
