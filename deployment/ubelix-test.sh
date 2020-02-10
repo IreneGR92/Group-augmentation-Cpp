b40ab43fdeb6542b162bb732b43bc0c7c089698b
IreneGR <irene.garcia@iee.unibe.ch>
2020-02-10 11:57:31 +0100

    ubelix example file

--- deployment/ubelix-test.sh
+++ deployment/ubelix-test.sh
@@ -1,93 +1,45 @@
 #!/bin/bash
-#
-#SBATCH --mail-type=none
-#SBATCH --job-name="test"
-#SBATCH --time=02:00:00
-#SBATCH --mem-per-cpu=8G
+
+# You must specify a valid email address!
+#SBATCH --mail-user=ig17c521@campus.unibe.ch
+
+# Mail on NONE, BEGIN, END, FAIL, REQUEUE, ALL
+#SBATCH --mail-type=end,fail
+
+# Job name
+#SBATCH --job-name="Example Job"
+
+# Runtime and memory
+#SBATCH --time=01:00:00
+#SBATCH --mem-per-cpu=2G
+
+# Partition
+#SBATCH --partition=all
+
+# For parallel jobs
+##SBATCH --cpus-per-task=8
+##SBATCH --nodes=2
+##SBATCH --ntasks=8
+##SBATCH --ntasks-per-node=4
+
+
+##SBATCH --output=/path/to/outfile
+##SBATCH --error=/path/to/errfile
+
+# For array jobs
+# Array job containing 100 tasks, run max 10 tasks at the same time
+##SBATCH --array=1-100%10
+
+#### Your shell commands below this line ####
+
+
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-declare -a arr=("init-No_relatedness.yml"
-		"07-Xh04-Xn02-No_relatedness.yml"
-		"X07-Xh02-Xn02-No_relatedness.yml"
-		"X07-Xh02-Xn0-No_relatedness.yml"
-		"X05-Xh04-Xn04-No_relatedness.yml"
-		"X05-Xh02-Xn04-No_relatedness.yml"
-		"K2-No_relatedness.yml"
-		"K2-Xn02-No_relatedness.yml"
-		"K2-Xh02-Xn02-No_relatedness.yml"
-		"K2-Xh02-Xn0-No_relatedness.yml"
-		"K2-X05-No_relatedness.yml"
-		"K2-X05-Xh02-No_relatedness.yml"
-		"bias10-No_relatedness.yml"
-		"bias10-Xn02-No_relatedness.yml"
-		"bias10-Xh02-Xn02-No_relatedness.yml"
-		"bias10-Xh02-Xn0-No_relatedness.yml"
-		"bias10-X05-No_relatedness.yml"
-		"bias10-X05-Xh02-No_relatedness.yml"
-		"bias10-X05-Xh02-Xn02-No_relatedness.yml"
-		"bias10-K2-No_relatedness.yml"
-		"bias10-K2-Xn02-No_relatedness.yml"
-		"bias10-K2-Xh02-Xn02-No_relatedness.yml"
-		"bias10-K2-Xh02-Xn0-No_relatedness.yml"
-		"bias10-K2-X05-No_relatedness.yml"
-		"bias10-K2-X05-Xh02-No_relatedness.yml"
-		"bias10-K2-X05-Xh02-Xn02-No_relatedness.yml"
+declare -a arr=(
 
-		"init-NRN.yml"
-		"X07-Xh04-Xn02-NRN.yml"
-		"X07-Xh02-Xn02-NRN.yml"
-		"X07-Xh02-Xn0-NRN.yml"
-		"X05-Xh04-Xn04-NRN.yml"
-		"X05-Xh02-Xn04-NRN.yml"
-		"K2-NRN.yml"
-		"K2-Xn02-NRN.yml"
-		"K2-Xh02-Xn02-NRN.yml"
-		"K2-Xh02-Xn0-NRN.yml"
-		"K2-X05-NRN.yml"
-		"K2-X05-Xh02-NRN.yml"
-		"bias10-NRN.yml"
-		"bias10-Xn02-NRN.yml"
-		"bias10-Xh02-Xn02-NRN.yml"
-		"bias10-Xh02-Xn0-NRN.yml"
-		"bias10-X05-NRN.yml"
-		"bias10-X05-Xh02-NRN.yml"
-		"bias10-X05-Xh02-Xn02-NRN.yml"
-		"bias10-K2-NRN.yml"
-		"bias10-K2-Xn02-NRN.yml"
-		"bias10-K2-Xh02-Xn02-NRN.yml"
-		"bias10-K2-Xh02-Xn0-NRN.yml"
-		"bias10-K2-X05-NRN.yml"
-		"bias10-K2-X05-Xh02-NRN.yml"
-		"bias10-K2-X05-Xh02-Xn02-NRN.yml"
-
-		"init-RN.yml"
-		"X07-Xh04-Xn02-RN.yml"
-		"X07-Xh02-Xn02-RN.yml"
-		"X07-Xh02-Xn0-RN.yml"
-		"X05-Xh04-Xn04-RN.yml"
-		"X05-Xh02-Xn04-RN.yml"
-		"K2-RN.yml"
-		"K2-Xn02-RN.yml"
-		"K2-Xh02-Xn02-RN.yml"
-		"K2-Xh02-Xn0-RN.yml"
-		"K2-X05-RN.yml"
-		"K2-X05-Xh02-RN.yml"
-		"bias10-RN.yml"
-		"bias10-Xn02-RN.yml"
-		"bias10-Xh02-Xn02-RN.yml"
-		"bias10-Xh02-Xn0-RN.yml"
-		"bias10-X05-RN.yml"
-		"bias10-X05-Xh02-RN.yml"
-		"bias10-X05-Xh02-Xn02-RN.yml"
-		"bias10-K2-RN.yml"
-		"bias10-K2-Xn02-RN.yml"
-		"bias10-K2-Xh02-Xn02-RN.yml"
-		"bias10-K2-Xh02-Xn0-RN.yml"
-		"bias10-K2-X05-RN.yml"
-		"bias10-K2-X05-Xh02-RN.yml"
-		"bias10-K2-X05-Xh02-Xn02-RN.yml")
-
+		"default.yml"
+)
 
 for i in "${arr[@]}"
 do
