45f7dd7418dd67423a059cf8c20a7a75d836da7a
IreneGR92 <irene.garcia@iee.unibe.ch>
2020-06-26 17:10:18 +0200

    all container functions seem to work properly

--- src/test.cpp
+++ src/test.cpp
@@ -4,42 +4,52 @@
 /* MAIN PROGRAM */
 int main(int count, char **argv) {
 
-    Parameters *parameters = new Parameters();
-
     auto *test = new Container<int>();
     auto *test2 = new Container<int>();
-//
-////    auto *test = new Container<Parameters>();
-//
+
+//    auto *test = new Container<Parameters>();
+
     int four = 4;
     int two = 2;
-//    int one = 1;
-//
-//    if (test->isEmpty()){std::cout << "vector empty" << std::endl;} else{std::cout << "vector NOT empty"<< std::endl;};
-//    test->add(four);
+    int one = 1;
+
+    if (test->isEmpty()){std::cout << "vector empty" << std::endl;} else{std::cout << "vector NOT empty"<< std::endl;};
+    test->add(four);
     test->add(two);
-//    test->add(one);
-//    test->remove(1); //by pointer instead of index if possible
-//    test->add(two);
-//    if (test->isEmpty()){std::cout << "vector empty"<< std::endl;} else{std::cout << "vector NOT empty"<< std::endl;};
-//    std::cout << test->accessElement(0)<< std::endl;
-//
-//
-    test->add(four);
-    test->add(four);
-    test->add(four);
-    test->add(four);
-//    test2->add(four);
-//    test->merge(*test2);
-//
-    std::cout << test->getRandomElement();
-//
-//    //int i = test->getRandomElement();
+    test->add(one);
+    test->remove(1); //by pointer instead of index if possible
+    test->add(two);
+    if (test->isEmpty()){std::cout << "vector empty"<< std::endl;} else{std::cout << "vector NOT empty"<< std::endl;};
+    std::cout << test->accessElement(0)<< std::endl;
+
+    test2->add(four);
+    test2->add(four);
+    test2->add(four);
+    test2->add(four);
+    test2->add(four);
+    test->merge(*test2);
+
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+    std::cout << test->getRandomElement() << std::endl;
+
+    int i = test->getRandomElement();
 
     std::cout << "all seems good!";
 
     delete test;
-//    delete test2;
+    delete test2;
 
     return 0;
 }
