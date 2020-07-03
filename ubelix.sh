a14dc73482aecb2f7ab887259085e2b9caaeada6
IreneGR92 <irene.garcia@iee.unibe.ch>
2020-07-03 17:19:26 +0200

    Revert "longer runs"
    This reverts commit f43a8651

--- ubelix.sh
+++ ubelix.sh
@@ -10,7 +10,7 @@
 #SBATCH --job-name="GroupAugmentation"
 
 # Runtime and memory
-#SBATCH --time=60:00:00 //TODO:check
+#SBATCH --time=90:00:00
 #SBATCH --mem-per-cpu=2G
 #SBATCH --cpus-per-task=1
 
